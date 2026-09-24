6fa1376c28237fd6117af0910b7714b04c53ee84
abdelrhman <abdelrhman.f.beseiso@gmail.com>
2026-09-24 10:53:53 +0300

    fix WSGI

--- typing_test_django/wsgi.py
+++ typing_test_django/wsgi.py
@@ -1,5 +1,12 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typing_test_django.settings')
+project_path = "/home/abood1dev/typing_race"
+if project_path not in sys.path:
+    sys.path.insert(0, project_path)
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "typing_test_django.settings"
+
+os.environ["DJANGO_SECRET_KEY"] = "jpp3fywqj=21^7-h0u+p^k^&_!o)x*$_jrcm&&jp$nx*fwj6vq"
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
