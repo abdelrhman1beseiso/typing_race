c78fc88cef900ea58aa0c4a136fc2e18c965364a
abdelrhman <abdelrhman.f.beseiso@gmail.com>
2026-09-24 10:59:03 +0300

    fix WSGI&settings

--- typing_test_django/settings.py
+++ typing_test_django/settings.py
@@ -1,24 +1,19 @@
 from pathlib import Path
-import os
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = 'django-insecure-typing-test-dev-key-change-in-production'
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["abood1dev.pythonanywhere.com"]
-
-CSRF_TRUSTED_ORIGINS = ["https://abood1dev.pythonanywhere.com"]
-
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.staticfiles",
-    "core",
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -43,7 +38,6 @@ TEMPLATES = [
         },
     },
 ]
-
 
 WSGI_APPLICATION = 'typing_test_django.wsgi.application'
 
--- typing_test_django/wsgi.py
+++ typing_test_django/wsgi.py
@@ -1,12 +1,5 @@
 import os
-import sys
-
-project_path = "/home/abood1dev/typing_race"
-if project_path not in sys.path:
-    sys.path.insert(0, project_path)
-
-os.environ["DJANGO_SETTINGS_MODULE"] = "typing_test_django.settings"
-
-os.environ["DJANGO_SECRET_KEY"] = "jpp3fywqj=21^7-h0u+p^k^&_!o)x*$_jrcm&&jp$nx*fwj6vq"
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typing_test_django.settings')
 application = get_wsgi_application()
